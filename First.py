76e2da02add234579c49c3d2bc7dc249682eb740
Arty <artyvacuum@ukr.net>
2022-11-27 09:07:10 +0300

    a

---
+++ First.py
@@ -0,0 +1,15 @@
+number = 5 # int
+
+digit = 4.54387 # float
+word = "Result:" # string
+boolean = True # bool
+
+str_num = "5" # string
+
+# print(word + str(digit))
+
+print(word + str(number + int(str_num)))
+del number
+number = 7
+
+print("Result:",number)
